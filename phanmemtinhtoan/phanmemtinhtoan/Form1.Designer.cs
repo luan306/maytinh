a5f7433f28eeddaeef3848dea6b0c4f793e0e7fe
luan306 <nguyenluan3062001@gmail.com>
2021-08-16 19:36:13 +0700

    first commit

---
+++ phanmemtinhtoan/phanmemtinhtoan/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace phanmemtinhtoan
+{
+    partial class MayTinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_b = new System.Windows.Forms.TextBox();
+            this.tb_a = new System.Windows.Forms.TextBox();
+            this.tb_kq = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(259, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Máy Tính";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(123, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nhập a :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(122, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nhập b :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(122, 210);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Kết Quả :";
+            // 
+            // tb_b
+            // 
+            this.tb_b.Location = new System.Drawing.Point(203, 166);
+            this.tb_b.Name = "tb_b";
+            this.tb_b.Size = new System.Drawing.Size(113, 20);
+            this.tb_b.TabIndex = 1;
+            // 
+            // tb_a
+            // 
+            this.tb_a.Location = new System.Drawing.Point(203, 120);
+            this.tb_a.Name = "tb_a";
+            this.tb_a.Size = new System.Drawing.Size(113, 20);
+            this.tb_a.TabIndex = 1;
+            // 
+            // tb_kq
+            // 
+            this.tb_kq.Location = new System.Drawing.Point(203, 209);
+            this.tb_kq.Name = "tb_kq";
+            this.tb_kq.Size = new System.Drawing.Size(115, 20);
+            this.tb_kq.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(371, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 26);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.reset_btn);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(126, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cộng ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.total_btn);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(228, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 26);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Trừ";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.tru_btn);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(371, 171);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 26);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Thoát";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Thoat_btn);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(330, 254);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 26);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Nhân";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.nhan_btn);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(432, 254);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(96, 26);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Chia";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.chia_btn);
+            // 
+            // MayTinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(677, 345);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_a);
+            this.Controls.Add(this.tb_kq);
+            this.Controls.Add(this.tb_b);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "MayTinh";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_b;
+        private System.Windows.Forms.TextBox tb_a;
+        private System.Windows.Forms.TextBox tb_kq;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ phanmemtinhtoan/phanmemtinhtoan/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace phanmemtinhtoan
+{
+    
+    public partial class MayTinh : Form
+    {
+        int a, b;
+        public MayTinh()
+        {
+            InitializeComponent();
+        }
+
+        private void reset_btn(object sender, EventArgs e)
+        {
+            tb_a.ResetText();
+            tb_b.ResetText();
+            tb_kq.ResetText();
+        }
+
+        private void total_btn(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(tb_a.Text);
+            b = Convert.ToInt32(tb_b.Text);
+            tb_kq.Text = Convert.ToString(a + b);
+
+        }
+
+        private void tru_btn(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(tb_a.Text);
+            b = Convert.ToInt32(tb_b.Text);
+            tb_kq.Text = Convert.ToString(a - b);
+
+        }
+
+        private void nhan_btn(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(tb_a.Text);
+            b = Convert.ToInt32(tb_b.Text);
+            tb_kq.Text = Convert.ToString(a * b);
+        }
+
+        private void chia_btn(object sender, EventArgs e)
+        {
+            a = Convert.ToInt32(tb_a.Text);
+            b = Convert.ToInt32(tb_b.Text);
+            tb_kq.Text = Convert.ToString(a / b);
+        }
+
+        private void Thoat_btn(object sender, EventArgs e)
+        {
+            DialogResult help;
+            help = (MessageBox.Show("Bạn có muốn thoát hay không ?" ,"Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning));
+            if (help == DialogResult.Yes)
+                Application.Exit();
+        }
+    }
+}
